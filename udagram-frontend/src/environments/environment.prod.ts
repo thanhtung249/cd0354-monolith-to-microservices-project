9aabb48568535073f7ae150126c32092ccd2acbc
tungbach <thanhtung249@gmail.com>
2023-07-07 23:57:02 +0700

    add screenshots

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aec172de6cf494c798215d32b136846e-1122600865.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
